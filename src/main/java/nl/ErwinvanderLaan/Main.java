4d61c8ed8eaf82cb13909370691b8717bbeb54fb
ErwinvanderLaan <erwinvanderlaan@gmail.com>
2024-09-30 10:30:49 +0200

    Added a scanner class, so users can input whole names and ages

--- src/main/java/nl/ErwinvanderLaan/Main.java
+++ src/main/java/nl/ErwinvanderLaan/Main.java
@@ -4,20 +4,24 @@ package nl.ErwinvanderLaan;
 
 import java.util.HashMap;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args)
-    throws java.io.IOException {
+        throws java.io.IOException {
         char choice, ignore ;
         HashMap<String, Integer> listOfPersons = new HashMap<String, Integer>();
+        Scanner userInput = new Scanner(System.in);
 
         for (; ; ) {
             // Three choices for the user.
             do {
-                System.out.println("Choose one");
-                System.out.println("  1. add a person");
-                System.out.println("  2. delete a person");
-                System.out.println("  3. show all persons");
-                System.out.print("Choose one (type 'q' to quit): ");
+                System.out.println("""
+                Choose one
+                1. add a person
+                2. delete a person
+                3. show all persons
+                Or type 'q' to quit)""");
 
                 choice = (char) System.in.read();
 
@@ -28,44 +32,45 @@ public class Main {
 
             if (choice == 'q') break;
 
-            System.out.println(" \n ");
-
             // A switch for each of the three choices.
             switch(choice) {
                 // adding a person to the HashMap by typing their name and age. Name = key, Age = value.
                 case '1':
-                    System.out.println("Please enter the name of the person you want to add ");
-                    char x = (char) System.in.read();
-                    String name = Character.toString(x);
-                    do {
-                        ignore = (char) System.in.read();
-                    } while (ignore != '\n');
+                    // Asking the user to enter the name of the person to be added.
+                    System.out.println("Please enter the name and age of the person you want to add ");
 
-                    System.out.println("Please enter the age of the person you want to add ");
-                    char a = (char) System.in.read();
-                    int age = a - '0';
+                    String name = userInput.nextLine();
+                    int age = userInput.nextInt();
 
-                    do {
-                        ignore = (char) System.in.read();
-                    } while (ignore != '\n');
+                    System.out.println("The name added is: " + name + "\n");
+                    System.out.println("The age added is: " + age + "\n");
 
+                    // Puts the entered name and age in the HashMap.
                     listOfPersons.put(name, age);
+
+                    // Clears the Scanner of current input in case the user wants to input a new person.
+                    userInput.nextLine();
+
                     break;
 
                 case '2':
                     // Removing a person from the HashMap by typing their name
                     System.out.println("Please enter the name of the person you want to remove ");
-                    char y = (char)System.in.read();
-                    String z = Character.toString(y);
-                    listOfPersons.remove(z);
+
+                    String removeName = userInput.nextLine();
+
+                    System.out.println("Name to be removed is: " + removeName + "\n");
+
+                    listOfPersons.remove(removeName);
+
                     break;
 
                 case '3':
                     // Show the total person file to the user by printing the Hashmap.
-                    System.out.println(listOfPersons);
+                    System.out.println("The list of added persons:");
+                    System.out.println(listOfPersons + "\n");
                     break;
             }
-            System.out.println(" \n ");
         }
     }
 }
